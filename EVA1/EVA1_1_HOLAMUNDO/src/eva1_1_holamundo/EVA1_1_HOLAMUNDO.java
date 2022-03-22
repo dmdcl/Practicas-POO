e838dfe64884f6982ce2ff71e60712d7c47dbf7b
Diego Martín del Campo <99349485+dmdcl@users.noreply.github.com>
2022-03-22 09:57:19 -0700

    Add files via upload

---
+++ EVA1/EVA1_1_HOLAMUNDO/src/eva1_1_holamundo/EVA1_1_HOLAMUNDO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_1_holamundo;
+
+/**
+ *
+ * @author user
+ */
+public class EVA1_1_HOLAMUNDO { //CUERPO DE UNA CLASE
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { //FUNCIÓN 
+        // TODO code application logic here
+        System.out.println("Hello world!");
+                    
+        }
+        
+}
