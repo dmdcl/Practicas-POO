19e8854f426b76be9952b5f9dc7b34b0cdc43d69
Diego Martín del Campo <99349485+dmdcl@users.noreply.github.com>
2022-05-02 09:43:37 -0600

    Add files via upload
    02/05/2022

--- EVA3/EVA3_1_STACK_OVERFLOW/src/eva3_1_stack_overflow/EVA3_1_STACK_OVERFLOW.java
+++ EVA3/EVA3_1_STACK_OVERFLOW/src/eva3_1_stack_overflow/EVA3_1_STACK_OVERFLOW.java
@@ -15,15 +15,19 @@ public class EVA3_1_STACK_OVERFLOW {
     /**
      * @param args the command line arguments
      */
+    public static int cont = 0;
     public static void main(String[] args) {
         // TODO code application logic here
         int x = 10;
         terminarStack();
     }
     
-    //stack es la memoria para variables locales 
-    //
+    //stack es la memoria para variables locales y llamadas
+    //a funcion
     public static void terminarStack(){
-        terminarStack();
+        cont = cont + 1; 
+        System.out.println(cont);
+        terminarStack();//recursividad
+                
     }
 }
