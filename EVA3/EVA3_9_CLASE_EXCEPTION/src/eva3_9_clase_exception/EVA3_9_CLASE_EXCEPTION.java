cc20ec5326eb44d6916d478426fd1ad00c64f5df
Diego Martín del Campo <99349485+dmdcl@users.noreply.github.com>
2022-05-10 09:49:28 -0600

    Add files via upload
    Clase 10/05/2022

---
+++ EVA3/EVA3_9_CLASE_EXCEPTION/src/eva3_9_clase_exception/EVA3_9_CLASE_EXCEPTION.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_9_clase_exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_9_CLASE_EXCEPTION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        //----------------
+        try{//AQUI VA EL CODIGO QUE PUEDE FALLAR
+            System.out.println("Introduce un numero entero 1:");
+            int entero = input.nextInt();
+            System.out.println("Introduce un numero entero 2:");
+            int entero2= input.nextInt();
+            System.out.println("Division: " + (entero/entero2));
+        }catch(ArithmeticException miVariable){//ACCION REMEDIAL
+        //miVariable.printStacktrace();//imprime la expecpion sin que termine el programa
+        System.out.println("Se produjo una division entre cero");
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("FIN DEL PROGRAMA");
+    }
+    
+}
