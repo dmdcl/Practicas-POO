8162f62f3a9a0db446ff934d5be8d4e01e22454a
Diego Martín del Campo <99349485+dmdcl@users.noreply.github.com>
2022-05-23 14:31:41 -0600

    Add files via upload

--- EVA3/EVA3_15_THROW_PERSONA/src/eva3_15_throw_persona/EVA3_15_THROW_PERSONA.java
+++ EVA3/EVA3_15_THROW_PERSONA/src/eva3_15_throw_persona/EVA3_15_THROW_PERSONA.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package eva3_15_throw_persona;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
- * @author invitado
+ * @author user
  */
 public class EVA3_15_THROW_PERSONA {
 
@@ -17,6 +19,65 @@ public class EVA3_15_THROW_PERSONA {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        boolean datoErroneo = true;
+        Persona persona = new Persona();
+        persona.setNombre("Juan Perez");
+        do {
+            try {
+                persona.setEdad(capturarEdad());
+                datoErroneo = false;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } while (datoErroneo);
+
     }
-    
+
+    public static int capturarEdad() throws Exception {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Captura la edad");
+        int valor = 0;
+        try {
+            valor = input.nextInt();
+        } catch (InputMismatchException ex) {
+            throw new Exception("Introdujiste una cadena de texto, no es un numero valido");
+        }
+        return valor;
+    }
+
+}
+
+class Persona {
+
+    private String nombre;
+    private int edad;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int edad) throws Exception {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) throws Exception {
+        if ((edad >= 0) && (edad <= 130)) {
+            this.edad = edad;
+        } else {
+            throw new Exception("Rango de edad incorrecto!!");
+        }
+    }
+
 }
