9c7cc3d88c65c3964b4fde3482dd8e71a8bb1822
Diego Martín del Campo <99349485+dmdcl@users.noreply.github.com>
2022-05-31 09:47:57 -0600

    Add files via upload

---
+++ EVA3/EVA3_21_SERIALIZABLE/src/eva3_21_serializable/EVA3_21_SERIALIZABLE.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_21_serializable;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_21_SERIALIZABLE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //binarios y de texto
+        //ESPECIFICAR QUE OBJETOS (CLASES) SON SERIALIZABLES
+        //DEBEMOS HACER QUE LA CLASE IMPLEMENTE LA INTERFAZ SERIALIZABLE
+        Persona persona = new Persona("Pedro", "Páramo");
+        escribirObj(persona);
+    }
+    
+    //GUARDAR OBJETOS
+    public static void escribirObj(Object obj){
+        try {
+            FileOutputStream abrirArch = new FileOutputStream("c://ARCHIVOS/ARCHIVO3.obj");
+            ObjectOutputStream guardarObj = new ObjectOutputStream(abrirArch);
+            guardarObj.writeObject(obj);
+            guardarObj.flush();
+            guardarObj.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(EVA3_21_SERIALIZABLE.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(EVA3_21_SERIALIZABLE.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    //LEER OBJETOS
+    
+}
+
+class Persona implements Serializable{
+    private String nombre, apellido;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+}
